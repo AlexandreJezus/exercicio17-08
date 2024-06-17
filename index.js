8b66c1afdd20fda4e7f6299ca15a05dcff024026
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-17 09:04:29 -0300

    feat: entrada usuario

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const usuario = { nome: "José", email: "jose11@email.com", idade: 25 };
+entrada = [];
+console.log("Digite o seu email para encontrar o seu cadastro.");
+process.stdin.on("data", function (data) {
+  entrada = data.toString().trim();
+  if (entrada == usuario.email) console.log(usuario);
+});
