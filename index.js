86502d6f14c3194cbfebb59c8d86355fcaa30c01
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-17 09:47:33 -0300

    feat entrada3

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ entrada = [];
 console.log("Digite o seu email para encontrar o seu cadastro.");
 process.stdin.on("data", function (data) {
   entrada = data.toString().trim();
-  if (entrada == usuario.email)
-    console.log("Seu email foi encontrado aqui está o seu cadastro. \n");
-  console.log(
-    "Seu nome é " +
-      usuario.nome +
-      "e você tem" +
-      usuario.idade +
-      "anos de idade."
-  );
+  if (entrada == usuario.email) {
+    console.log("Seu email foi encontrado aqui está o seu cadastro: \n");
+    console.log(
+      "Seu nome é " +
+        usuario.nome +
+        " e você tem " +
+        usuario.idade +
+        " anos de idade." +
+        "O seu email cadastrado é " +
+        usuario.email
+    );
+  } else console.log("O seu email está incorreto ou não foi encontrado.");
 });
